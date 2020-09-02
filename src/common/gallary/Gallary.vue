15031d6b58ad2c973128943654cafc27df79fbfe
1213William <1950423854@qq.com>
2020-09-02 23:55:53 +0800

    创建了公共组件，图片滚动

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+<!--    gallary-->
+    <div class="wrapper">
+      123
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name: 'CommonGallary'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .container
+    display flex
+    flex-direction column
+    justify-content center
+    
+    z-index 99
+    position fixed
+    left 0
+    right 0
+    top 0
+    bottom  0
+    background: #000
+    .wrapper
+      background: #fff
+
+</style>
+
