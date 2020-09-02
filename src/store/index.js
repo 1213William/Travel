fd962954c6e8bb09898ae6de8afd672d0357e4de
1213William <1950423854@qq.com>
2020-09-02 20:37:11 +0800

    --

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,18 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import state from './state'
+import mutations from './mutation'
+import actions from './action'
 
 Vue.use(Vuex)
 
-let defaultCity = '上海'
 
-try{
-    if (localStorage.city) {
-        defaultCity = localStorage.city
-    }
-}catch (e) {}
+
+
 
 export default new Vuex.Store({
-    state: {
-        city: defaultCity
-    },
-    actions: {
-        changeCity (ctx, city) {
-            // console.log(city)
-            ctx.commit('changeCity', city)
-        }
-    },
-    mutations: {
-        changeCity (state, city) {
-            state.city = city
-            try{
-                localStorage.city = city
-            }catch (e) {}
-
-        }
-    }
+    state,
+    actions,
+    mutations,
 })
 
