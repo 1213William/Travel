cf82a56fdf2d61cb2604420dc0272771b3b3709c
1213William <1950423854@qq.com>
2020-09-03 17:09:09 +0800

    添加了详情页中的奸淫艰辛的效果

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,23 +1,28 @@
 <template>
   <div>
     <detail-banner></detail-banner>
+    <detail-header></detail-header>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 import axios from 'axios'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
+    DetailHeader,
   },
 
 }
 </script>
 
 <style lang="stylus" scoped>
-
+  .content
+    height 50rem
 
 </style>
